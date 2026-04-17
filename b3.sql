e0f5c5d0c1ccf930397dbb455bcd8b4fc3797c42
hung20071612-hue <hung20071612@gmail.com>
2026-04-17 15:54:24 +0700

    Fix formatting of INSERT statements in b3.sql

--- b3.sql
+++ b3.sql
@@ -15,10 +15,10 @@ CREATE TABLE CUSTOMERS (
 
 INSERT INTO CUSTOMERS (FullName, Email, City, LastPurchaseDate, Status)
 VALUES
-    ('Nguyễn Văn A', 'anv@gmail.com', 'Hà Nội', '2025-05-20', 'Active'),     -- Khách hàng tiềm năng (hơn 6 tháng)
-    ('Trần Thị B', 'btt@gmail.com', 'Hà Nội', '2026-02-10', 'Active'),       -- Mới mua (gần đây)
-    ('Lê Văn C', NULL, 'Hà Nội', '2025-01-15', 'Active'),                    -- Lỗi: Không có Email
-    ('Phạm Minh D', 'dpm@gmail.com', 'Hà Nội', '2024-12-01', 'Locked'),      -- Lỗi: Tài khoản bị khóa
+    ('Nguyễn Văn A', 'anv@gmail.com', 'Hà Nội', '2025-05-20', 'Active'),     
+    ('Trần Thị B', 'btt@gmail.com', 'Hà Nội', '2026-02-10', 'Active'),       
+    ('Lê Văn C', NULL, 'Hà Nội', '2025-01-15', 'Active'),                    
+    ('Phạm Minh D', 'dpm@gmail.com', 'Hà Nội', '2024-12-01', 'Locked'),      
     ('Hoàng An E', 'eha@gmail.com', 'TP HCM', '2025-03-01', 'Active');   
 
 SELECT FullName, Email FROM CUSTOMERS
